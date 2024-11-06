40a05f6815f2e773a0b84ebb685c3dd3f49a387e
Luis (universidad) <luis@TEC-LLONDONO.mineraelroble.local>
2024-11-05 20:37:25 -0500

    Cambios de sintaxys , ; al final de cada return y correcion en el println con el +

--- CalculadoraSimple.java
+++ CalculadoraSimple.java
@@ -1,21 +1,22 @@
+
 public class CalculadoraSimple {
 
     public int sumar(int a, int b) {
-        return a + b 
+        return a + b;
     }
 
     public int multiplicar(int a, int b) {
-        int resultado = a * b
+        int resultado = a * b;
         return resultado;
     }
 
     public static void main(String[] args) {
         CalculadoraSimple calc = new CalculadoraSimple();
-        
+
         int resultadoSuma = calc.sumar(10, 5);
         int resultadoMultiplicacion = calc.multiplicar(10, 10);
 
-        System.out.println(Resultado de la suma: " + resultadoSuma);
+        System.out.println("Resultado de la suma: " + resultadoSuma);
         System.out.println("Resultado de la multiplicación: " + resultadoMultiplicacion);
     }
 }
